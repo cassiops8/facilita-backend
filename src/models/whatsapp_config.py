31124827a69f3d664244482a1bce22abd35e9228
cassiops8 <cassiops8@gmail.com>
2026-05-29 10:16:18 -0300

    Update whatsapp_config.py

--- src/models/whatsapp_config.py
+++ src/models/whatsapp_config.py
@@ -5,7 +5,7 @@ class WhatsAppConfig(db.Model):
     __tablename__ = 'whatsapp_configs'
     
     id = db.Column(db.Integer, primary_key=True)
-    cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.id'), nullable=False)
+    cliente_id = db.Column(db.Integer, nullable=True)
     numero_whatsapp = db.Column(db.String(20), nullable=False)
     nome_exibicao = db.Column(db.String(100), nullable=False)
     token_acesso = db.Column(db.String(500), nullable=True)
@@ -15,24 +15,19 @@ class WhatsAppConfig(db.Model):
     data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
     data_atualizacao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
-    # Relacionamentos
-    cliente = db.relationship('Cliente', backref=db.backref('whatsapp_configs', lazy=True))
-    
     def to_dict(self):
         return {
             'id': self.id,
             'cliente_id': self.cliente_id,
             'numero_whatsapp': self.numero_whatsapp,
             'nome_exibicao': self.nome_exibicao,
-            'token_acesso': self.token_acesso[:10] + '...' if self.token_acesso else None,  # Mascarar token
+            'token_acesso': self.token_acesso[:10] + '...' if self.token_acesso else None,
             'phone_number_id': self.phone_number_id,
             'webhook_verify_token': self.webhook_verify_token,
             'ativo': self.ativo,
             'data_criacao': self.data_criacao.isoformat() if self.data_criacao else None,
             'data_atualizacao': self.data_atualizacao.isoformat() if self.data_atualizacao else None,
-            'cliente_nome': self.cliente.nome if self.cliente else None
         }
     
     def __repr__(self):
         return f'<WhatsAppConfig {self.numero_whatsapp} - {self.nome_exibicao}>'
-
